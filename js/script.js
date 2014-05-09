90596814713f87cd2853193d20f8c881a1152fd8
puhrome <phirom.yim@fullsail.edu>
2014-05-09 16:57:34 -0400

    Increments Expressions
    Using WebStorm in displaying examples of Increments

--- js/script.js
+++ js/script.js
@@ -2,6 +2,21 @@
  * Increments Created by puhrome on 5/9/14.
  */
 //Increments ++myVariable
-myNumber = (prompt ("Enter Your Number"));
+
+console.log("++myVariable");
+
+myNumber = (prompt ("Enter Your First Number"));
 myVar = myNumber++ - 20;
 console.log (myNumber);
+
+console.log ("myNumber = myNumber + 1");
+
+myNumber2 = (prompt ("Enter Your Second Number"));
+myNumber2 = myNumber2 + 1;
+console.log(myNumber2);
+
+console.log("++myVariable");
+
+myNumber3 = (prompt ("Enter Your Third Number"));
+myVar3 = ++myNumber3 - 20;
+console.log(myNumber3);
